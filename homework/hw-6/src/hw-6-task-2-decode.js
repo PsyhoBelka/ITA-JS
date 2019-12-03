c69f325c60b76a7a9f657a803fa7f50d163c44d6
PsyhoBelka <agroghkov@gmail.com>
2019-12-03 16:53:02 +0200

    wip2 task2 hw6

--- homework/hw-6/src/hw-6-task-2-decode.js
+++ homework/hw-6/src/hw-6-task-2-decode.js
@@ -31,3 +31,37 @@ while (!stop) {
     i = c[i.length];
 }
 console.log(d);
+
+
+let str = 'Yesterday, we bumped into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.\n' +
+    'Yesterday, we bumped into Laura. It had to happen, but you can\'t deny the timing couldn\'t be worse. The "mission" to try and seduce her was a complete failure last month. By the way, she still has the ring I gave her. Anyhow, it hasn\'t been a pleasurable experience to go through it. I wanted to feel done with it first.'
+// console.log(str)
+let b = str.split(/\.|!|\?/)
+// console.log(b)
+let c = b.map(x => {
+    return x.toString()
+        .replace(/,|"|'|`/g, '').trim()
+}).filter(x => {
+    return x.length > 0
+})
+console.log(c.length)
+let d = c.join(' ')
+console.log(d)
+let i = 0
+let e = []
+let stop = false;
+while (!stop) {
+    console.log(i, c.length)
+    e.push(c[i].split(' '))
+    e = e.flat()
+    console.log(e)
+    if (i > c.length) {
+        stop = true
+        console.log(stop)
+    } else {
+        i += e.length + 1
+    }
+
+}
+
+console.log(e.flat());
